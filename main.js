6d79872a776add7cec8f999d3ec3e64d70c591fb
CullimoreMa550 <CullimoreMa550@northwested.org>
2024-04-24 10:01:25 -0400

    Finished

---
+++ main.js
@@ -0,0 +1,46 @@
+// Maddox C / 4-24-24 / JS Random Numbers PRIMM
+
+
+
+
+
+// //INPUT
+// const randNum1 = Math.random();
+// const randNum2 = Math.random();
+// const randNum3 = Math.random();
+
+// //PROCESS AND OUTPUT
+// console.log(`First Random # is ${randNum1}`);
+// console.log(`Second Random # is ${randNum2}`);
+// console.log(`Third Random # is ${randNum3}`);
+
+
+
+
+//-------------------------------------------------------------------------
+
+
+
+// const randNum1 = Math.random()*5;
+// const randNum2 = Math.random()*5;
+// const randNum3 = Math.random()*5;
+
+// console.log(`First: ${randNum1}`);
+// console.log(`Second: ${randNum3}`);
+// console.log(`Third: ${randNum2}`);
+
+
+
+
+//-------------------------------------------------------------------------
+
+
+
+
+const randNum1 = Math.floor(Math.random()*5);
+const randNum2 = Math.floor(Math.random()*5);
+const randNum3 = Math.floor(Math.random()*5);
+
+console.log(`First: ${randNum1}`);
+console.log(`Second: ${randNum3}`);
+console.log(`Third: ${randNum2}`);
